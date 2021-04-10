29c393a1e375cc623657232979f711336fee0054
Rizky Ian Indiarto <rizkyian78@gmail.com>
2021-04-11 00:31:50 +0700

    fix: try seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,15 +9,29 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          property="og:title"
-          content="Rizky Ian Indiarto"
-          key="Rizky Ian Indiarto"
-        />
-        <meta name="description" content="Rizky Ian Indiarto" />
         <title>Rizky Ian Indiarto || Political Graduate</title>
         <link rel="icon" href="/deerHead.png" />
+        <meta name="title" content="rizky ian indiarto" />
+        <meta name="description" content="rizky ian indiarto" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://website-portofolio-ian.vercel.app/"
+        />
+        <meta property="og:title" content="rizky ian indiarto" />
+        <meta property="og:description" content="rizky ian indiarto" />
+        <meta property="og:image" content="rizkyianindiarto" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://twitter.com/rizkyian1" />
+        <meta property="twitter:title" content="Komba kombi" />
+        <meta
+          property="twitter:description"
+          content="Suka sama film echhi love sora aoi and bokep"
+        />
+        <meta
+          property="twitter:image"
+          content="https://twitter.com/rizkyian1/photo"
+        />
       </Head>
       <Header />
       <Row justify="space-between" style={{ maxWidth: '100%' }}>
