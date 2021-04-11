c06cadbb5fe50872006039662cee8796449fe3ea
Rizky Ian Indiarto <rizkyian78@gmail.com>
2021-04-11 14:42:06 +0700

    fix: asdsadasd

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       <Head>
         <html lang="en" />
-        <title>Rizky Ian Indiarto || Political Graduate</title>
+        <title>Rizky Ian Indiarto</title>
         <link rel="icon" href="/deerHead.png" />
         <meta name="title" content="Rizky Ian Indiarto" />
         <meta
