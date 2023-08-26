d3b359690ad5d7960bc2e6fcb2d14616532026fc
Rizky Indiarto <rizky.indiarto@brank.as>
2023-08-27 03:52:12 +0700

    fix: asdasdasd

--- src/layouts/containers/Public/Footer/index.tsx
+++ src/layouts/containers/Public/Footer/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Content, { ContentProps } from '@nexys/components/Content/Content'
 import { Col, Row } from 'antd'
 import Text from '@nexys/components/Typography/Text'
-import LazyImgwrapper from '@nexys/components/LazyImgWrapper'
 
 function Footer(props: ContentProps) {
   return <></>
