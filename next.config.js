3bd58cc71cf73f3910f196d36c988a2da065e2c4
Rizky Indiarto <rizky.indiarto@brankas.com>
2024-11-03 23:44:16 +0700

    feat: add tm to fixing

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const withAntdLess = require('next-plugin-antd-less')
 const withSVG = require('./webpack-extends/svgr')
 
+const withTM = require('next-transpile-modules')(['@babel/runtime', 'rc-util']); // Add other packages if necessary
+
+
 const lessConfig = withAntdLess({
   lessVarsFilePath: './src/styles/variables.less',
   javascriptEnabled: true,
@@ -12,6 +15,6 @@ const lessConfig = withAntdLess({
 
 const svgConfig = withSVG(lessConfig)
 
-module.exports = {
+module.exports = withTM({
   ...svgConfig,
-}
+});
